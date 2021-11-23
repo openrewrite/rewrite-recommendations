37ab3b64ae9a09eabb2619197c67a785c4156859
Kyle Scully <scullykns@gmail.com>
2021-11-23 08:55:45 -0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+![Logo](https://github.com/openrewrite/rewrite/raw/main/doc/logo-oss.png)
+### Recommended Recipes
+
+[![ci](https://github.com/openrewrite/rewrite-testing-frameworks/actions/workflows/ci.yml/badge.svg)](https://github.com/openrewrite/rewrite-recommendations/actions/workflows/ci.yml)
+[![Apache 2.0](https://img.shields.io/github/license/openrewrite/rewrite-testing-frameworks.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+[![Maven Central](https://img.shields.io/maven-central/v/org.openrewrite.recipe/rewrite-recommendations.svg)](https://mvnrepository.com/artifact/org.openrewrite.recipe/rewrite-recommendations)
+
+### What is this?
+
+A [Rewrite module](https://github.com/openrewrite/rewrite) hosting recommended collections of recipes.
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "rewrite-recommendations"
+
+plugins {
+    id("com.gradle.enterprise") version "3.7"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.4.2"
+}
+
+gradleEnterprise {
+    val isCiServer = System.getenv("CI")?.equals("true") ?: false
+    server = "https://ge.openrewrite.org/"
+
+    buildCache {
+        remote(HttpBuildCache::class) {
+            url = uri("https://ge.openrewrite.org/cache/")
+            isPush = isCiServer
+        }
+    }
+
+    buildScan {
+        capture {
+            isTaskInputFiles = true
+        }
+
+        isUploadInBackground = !isCiServer
+
+        publishAlways()
+        this as com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+        publishIfAuthenticated()
+    }
+
+}
