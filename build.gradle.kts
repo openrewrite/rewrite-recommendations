3bf7e0726783f1ca815a383d1620f5060d1e89fd
Knut Wannheden <knut@moderne.io>
2023-06-05 07:43:50 +0200

    Fix wrong coordinate for `rewrite-static-analysis`

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,7 +106,7 @@ dependencies {
     implementation("org.openrewrite:rewrite-xml:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-hcl:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-maven:${rewriteVersion}")
-    implementation("org.openrewrite:rewrite-static-analysis:${rewriteVersion}")
+    implementation("org.openrewrite.recipe:rewrite-static-analysis:${rewriteVersion}")
 
     runtimeOnly("com.fasterxml.jackson.core:jackson-core:2.15.1")
 
