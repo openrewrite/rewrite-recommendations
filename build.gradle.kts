33779faa10a281fbd632dbd40a510433b75c5355
Tim te Beek <timtebeek@gmail.com>
2023-06-12 10:09:27 +0200

    Adopt org.openrewrite.build.recipe-library (#12)
    * Adopt org.openrewrite.build.recipe-library
    
    * Bump Gradle wrapper version
    
    * Use rewrite-recipe-bom and add rewrite-terraform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,201 +1,33 @@
-import nebula.plugin.contacts.Contact
-import nebula.plugin.contacts.ContactsExtension
-import nl.javadude.gradle.plugins.license.LicenseExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.util.*
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-    }
-}
-
 plugins {
-    `java-library`
-    `maven-publish`
-    signing
-
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
-    id("nebula.maven-resolved-dependencies") version "17.3.2"
-    id("nebula.release") version "15.3.1"
-    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
-
-    id("com.github.hierynomus.license") version "0.16.1"
-    id("com.github.jk1.dependency-license-report") version "1.16"
-    id("org.owasp.dependencycheck") version "7.0.4.1"
-
-    id("nebula.maven-publish") version "17.3.2"
-    id("nebula.contacts") version "5.1.0"
-    id("nebula.info") version "9.3.0"
-
-    id("nebula.javadoc-jar") version "17.3.2"
-    id("nebula.source-jar") version "17.3.2"
-    id("nebula.maven-apache-license") version "17.3.2"
-
-    id("org.openrewrite.rewrite") version "latest.release"
-}
-
-rewrite {
-    activeRecipe("org.openrewrite.java.format.AutoFormat", "org.openrewrite.staticanalysis.CodeCleanup")
-}
-
-apply(plugin = "nebula.publish-verification")
-
-configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
-    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-dependencyCheck {
-    analyzers.assemblyEnabled = false
-    suppressionFile = "suppressions.xml"
-    failBuildOnCVSS = 9.0F
+    id("org.openrewrite.build.recipe-library") version "latest.release"
 }
 
 group = "org.openrewrite.recipe"
-description =
-    "A rewrite module hosting recommended collections of recipes"
+description = "A rewrite module hosting recommended collections of recipes"
 
 repositories {
-    if(!project.hasProperty("releasing")) {
-        mavenLocal()
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+    val maven = maven {
+        url = uri("https://repo.gradle.org/gradle/libs-releases/")
+        content {
+            excludeVersionByRegex(".+", ".+", ".+-rc-?[0-9]*")
         }
     }
-    mavenCentral()
-}
-
-nexusPublishing {
-    repositories {
-        sonatype()
+    // Needed to pick up snapshot versions of rewrite
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
-signing {
-    setRequired({
-        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
-    })
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications["nebula"])
-}
-
-configurations.all {
-    resolutionStrategy {
-        cacheChangingModulesFor(0, TimeUnit.SECONDS)
-        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
-    }
-}
-
-var rewriteVersion = if(project.hasProperty("releasing")) {
-    "latest.release"
-} else {
-    "latest.integration"
-}
-
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 dependencies {
-    constraints {
-        implementation("com.fasterxml.woodstox:woodstox-core:6.5.1") {
-            because("Versions <= 6.3.1")
-        }
-    }
+    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:$rewriteVersion"))
 
-    implementation("org.openrewrite:rewrite-yaml:$rewriteVersion")
-    implementation("org.openrewrite:rewrite-java:${rewriteVersion}")
-    implementation("org.openrewrite:rewrite-xml:${rewriteVersion}")
-    implementation("org.openrewrite:rewrite-hcl:${rewriteVersion}")
-    implementation("org.openrewrite:rewrite-maven:${rewriteVersion}")
-    implementation("org.openrewrite.recipe:rewrite-static-analysis:${rewriteVersion}")
+    implementation("org.openrewrite:rewrite-yaml")
+    implementation("org.openrewrite:rewrite-java")
+    implementation("org.openrewrite:rewrite-xml")
+    implementation("org.openrewrite:rewrite-hcl")
+    implementation("org.openrewrite:rewrite-maven")
+    implementation("org.openrewrite.recipe:rewrite-terraform")
+    implementation("org.openrewrite.recipe:rewrite-static-analysis")
 
-    runtimeOnly("com.fasterxml.jackson.core:jackson-core:2.15.1")
-
-    compileOnly("org.projectlombok:lombok:latest.release")
-    annotationProcessor("org.projectlombok:lombok:latest.release")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.openrewrite:rewrite-test:$rewriteVersion")
-    testImplementation("org.assertj:assertj-core:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
-}
-
-tasks.withType(KotlinCompile::class.java).configureEach {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-    jvmArgs = listOf("-Xmx1g", "-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
-}
-
-tasks.named<JavaCompile>("compileJava") {
-    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-    targetCompatibility = JavaVersion.VERSION_1_8.toString()
-
-    options.isFork = true
-    options.compilerArgs.addAll(listOf("--release", "8"))
-}
-
-configure<ContactsExtension> {
-    val j = Contact("team@moderne.io")
-    j.moniker("Moderne")
-
-    people["team@moderne.io"] = j
-}
-
-configure<LicenseExtension> {
-    ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
-    skipExistingHeaders = true
-    header = project.rootProject.file("gradle/licenseHeader.txt")
-    mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
-    strictCheck = true
-}
-
-configure<PublishingExtension> {
-    publications {
-        named("nebula", MavenPublication::class.java) {
-            suppressPomMetadataWarningsFor("runtimeElements")
-
-            pom.withXml {
-                (asElement().getElementsByTagName("dependencies").item(0) as? org.w3c.dom.Element)?.let { dependencies ->
-                    dependencies.getElementsByTagName("dependency").let { dependencyList ->
-                        var i = 0
-                        var length = dependencyList.length
-                        while (i < length) {
-                            (dependencyList.item(i) as? org.w3c.dom.Element)?.let { dependency ->
-                                if ((dependency.getElementsByTagName("scope")
-                                        .item(0) as? org.w3c.dom.Element)?.textContent == "provided"
-                                ) {
-                                    dependencies.removeChild(dependency)
-                                    i--
-                                    length--
-                                }
-                            }
-                            i++
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-tasks.withType<Javadoc> {
-    // assertTrue(boolean condition) -> assertThat(condition).isTrue()
-    // warning - invalid usage of tag >
-    // see also: https://blog.joda.org/2014/02/turning-off-doclint-in-jdk-8-javadoc.html
-    (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.compilerArgs.add("-parameters")
 }
